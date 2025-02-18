c9c7425c6c23c6934c56ae24401ed798802eb85c
Prosefionol <ilya_kuper@mail.ru>
2025-02-18 22:00:43 +0300

    create empty project

---
+++ app/src/main/java/com/example/catube/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.catube
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.catube.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+}
